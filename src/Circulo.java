5dd1609c24c012536673b036e14ca56322f720d3
zubiri <zubiri@Ubuntu-12-04.(none)>
2014-09-25 08:38:31 +0200

    version 1.5

--- src/Circulo.java
+++ src/Circulo.java
@@ -7,16 +7,16 @@ public class Circulo {
 public void setRadio(double radio) 
  {
 	this.radio = radio;
-	this.area = 3.1416*radio*radio;
-	this.circu = 2*3.1416*radio;
+	area = 3.1416*radio*radio;
+	circu = 2*3.1416*radio;
  }
  
-  public double getArea() 
+public double getArea() 
  {	
 	return area;
  }
  
- public double getCircu() 
+public double getCircu() 
  {	
 	return circu;
  }
