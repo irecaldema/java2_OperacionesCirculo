447255731adfb3eb890d1efe60f8b0b579309833
zubiri <irecaldema14dw@ikzubirimanteo.com>
2014-10-07 06:20:23 +0000

    version constructor

--- src/OperacionesCirculo.java
+++ src/OperacionesCirculo.java
@@ -13,10 +13,7 @@ public static void main (String args[])
  
  Circulo operaciones = new Circulo(radio);
   
- //resultado = operaciones.area(radio);
  System.out.println("\nEl area del circulo es: " + operaciones.Area());
-
- //resultado = operaciones.circu(radio); 
  System.out.println("\nLa circunferencia del circulo es: " + operaciones.Circu());
 
  System.out.println("\n\nEGUN ONA IZAN!!\n");
